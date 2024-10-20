3713e026d30d5c0a54ea4fee30208befaeb91b59
Carlos A. Carro Duplá <ccarrodupla@gmail.com>
2024-10-20 19:36:18 -0400

    Add a new solution for the Anagram exercise

---
+++ exercises/1_2_anagrams/solution_3.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+def anagram(word)
+  dictionary_file = File.join(__dir__, 'wl.txt')
+  word_size = word.size
+  # Only words with the size as the target:
+  small_dictionary = File.open(dictionary_file).readlines("\n").
+                          collect(&:chomp).
+                          select{|line| line.size == word_size}
+  word_array = word.chars.sort  # 'zulu' => ["l", "u", "u", "z"]
+  small_dictionary.collect{|w| w.chars.sort}.
+                   collect.with_index{|w, i| w == word_array ? small_dictionary[i] : nil}.
+                   compact
+end
