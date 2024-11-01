5ac5c38c86a94d71232fa1e73248b4562de7907a
Carlos A. Carro Duplá <ccarrodupla@gmail.com>
2024-10-31 20:28:39 -0400

    New exercise: LOGO turtle intersection

---
+++ exercises/4_intersection/test_data.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+SOLUTION_METHOD_NAME = 'solution'
+TEST_DATA = {
+              [1, 3, 2, 5, 4, 4, 6, 3, 2] => 7
+            }
+TEST_FAILURE_EXTRA_DETAILS = ->(arg) { '' }
