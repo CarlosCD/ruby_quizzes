751cddbb0dfca7ebb4d96c8ff252c0b8daed62da
Carlos A. Carro Duplá <ccarrodupla@gmail.com>
2024-11-04 15:49:57 -0500

    Caesar Cipher, faulty solution number 1

--- exercises/7_caesar_cipher_variation_1/solution.rb
+++ exercises/7_caesar_cipher_variation_1/solution.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 def encode_str(a)
-  # your solution here
+  # Allow String as an argument:
+  arr = a.is_a?(String) ? (eval a rescue []) : a
+  return a if !arr.is_a?(Array) || arr.compact.size != 2
+  u, n = arr
+  n = n.to_i
+  # u is the message to be encoded
+  # n is the shift
+  return [] if !u.is_a?(String) || u.empty?
+  a = u.b # ASCII, just in case
+  first = a[0].downcase
+  encoded = [first, (first.getbyte(0) + n).chr].join
+  a.each_char do |c|
+    encoded << ((c =~ /[a-z]/i) ? (c.getbyte(0) + n).chr : c)
+  end
+  max_length = (encoded.size / 5.0).ceil(0)
+  encoded.chars.each_slice(max_length).map(&:join)
 end
--- exercises/7_caesar_cipher_variation_2/solution.rb
+++ exercises/7_caesar_cipher_variation_2/solution.rb
@@ -1,5 +1,16 @@
-# frozen_string_literal: true
-
+# Decodes an Array of five strings:
 def decode(a)
-  # your solution here
+  # Allow String as an argument:
+  arr = a.is_a?(String) ? (eval a rescue []) : a
+  # Verify the argument is valid:
+  return arr if !arr.is_a?(Array) || arr.size < 1 || arr.size > 5 || !arr.first.is_a?(String) || arr.first.size <= 2
+  encoded = arr.join.b # ASCII String
+  prefix = encoded.slice!(0,2).bytes
+  n = prefix[1] - prefix[0]
+  decoded = ''
+  encoded.each_char do |c|
+    dec_char = (c.getbyte(0) - n).chr
+    decoded << ((dec_char =~ /[a-z]/i) ? dec_char : c)
+  end
+  decoded
 end
